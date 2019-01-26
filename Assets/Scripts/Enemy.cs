059a555bcc27dc81b355c0a3a888b40ab791e19d
paos <>
2019-01-26 16:55:07 -0500

    Player and enemies spawn properly

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -19,6 +19,8 @@ public class Enemy : MonoBehaviour
     private AudioSource _audioSource;
     private Rigidbody2D _rigidbody2D;
     private BoxCollider2D _boxCollider2D;
+
+    private Camera _cam;
     
     // Start is called before the first frame update
     protected void Start()
@@ -28,6 +30,8 @@ public class Enemy : MonoBehaviour
         _audioSource = GetComponent<AudioSource>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _boxCollider2D = GetComponent<BoxCollider2D>();
+        _cam = Camera.main;
+        transform.position = RandomSpawnLocation();
     }
 
     // Update is called once per frame
@@ -40,8 +44,9 @@ public class Enemy : MonoBehaviour
     {
         transform.Translate(Vector3.right * _speed * Time.deltaTime);
     }
-    
-    
-    
-    
+
+    private Vector3 RandomSpawnLocation()
+    {
+        return _cam.ScreenToWorldPoint(new Vector3(-50, Random.Range(0, Screen.height-10), _cam.nearClipPlane));
+    }
 }
--- Assets/Scripts/GameScene.cs
+++ Assets/Scripts/GameScene.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
 
 public class GameScene : Scene
@@ -7,13 +8,27 @@ public class GameScene : Scene
 //    private GameObject 
     // Start is called before the first frame update
     private GameObject _threshold;
-    private GameObject _enemy;
+    [SerializeField]public float _roundTimeInSeconds;
+    private GameObject _player;
+    private Camera _cam;
 
     void Start()
     {
-        Services.GameManager.Player = Services.GameManager.CreateGameObject(Services.PrefabDatabase.Actors[0]);
+        _cam = Camera.main;
+        _player = Services.GameManager.CreateGameObject(Services.PrefabDatabase.Actors[0]);
+        _player.transform.position = _cam.ScreenToWorldPoint(new Vector3(_cam.pixelWidth * 0.90f, Random.Range(0, Screen.height-10), _cam.nearClipPlane));
         Services.EnemyManager = FindObjectOfType<EnemyManager>();
     }
 
     // Update is called once per frame
+    void Update()
+    {
+        Services.EnemyManager.EmitWave(CountdownRoundTime());
+    }
+
+    public float CountdownRoundTime()
+    {
+        _roundTimeInSeconds -= Time.deltaTime;
+        return _roundTimeInSeconds;
+    }
 }
