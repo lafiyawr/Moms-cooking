99a4acc8a137872928491f2080ebc6d020896024
lafiyawr <lafiyaw@gmail.com>
2019-01-26 13:15:29 -0500

    Revert "player"

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,40 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
-    public float speed = 1.0f;
-
-    public GameObject weapon;
-    public Transform weaponTransform;
     // Start is called before the first frame update
     void Start()
     {
-      
+        Debug.Log("HELLO! I'm a player!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.position += Vector3.up * Time.deltaTime * speed;
-           // Debug.Log(transform.rotation.z);
-        }
-
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.position += Vector3.down * Time.deltaTime * speed;
-            // Debug.Log(transform.rotation.z);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(weapon, weaponTransform.position, weaponTransform.rotation);
-
-        }
-
-
+        Debug.Log("Player here!");
+        
     }
-
-
 }
